f2a3520d4585337a7f04ddae16e9613ee545291d
Samuel Albert <samuel.albert@centraliens.net>
2024-12-07 03:54:55 +0100

    Getting the rewards info available. Passing `rewardName` onto the donation event so the activity feed message gives reward name. Passing more data into the data structure of the reward to prepare for using them more.
    Small syntax fixes.

--- src/types/campaign-reward.ts
+++ src/types/campaign-reward.ts
@@ -1,4 +1,11 @@
+import { TiltifyMoney } from "./shared";
+
 export type TiltifyCampaignReward = {
     id: string;
     name: string;
+    amount: TiltifyMoney;
+    active: boolean;
+    description: string;
+    quantity: number;
+    quantity_remaining: number;
 };
