058134556935fcc8ae13ea1d341b9fcd41b924af
Alastor <Samuel38@users.noreply.github.com>
2024-12-09 05:52:07 +0100

    Rising Firebot event "Milestone Reached" upon reaching a milestone Saving known milestones state so we remember which events have been raised between bot restarts. Updating the list of milestones each time a milestone is reached.
    Refers to #7

--- src/events/tiltify-event-source.ts
+++ src/events/tiltify-event-source.ts
@@ -1,8 +1,11 @@
 import { EventSource } from "@crowbartools/firebot-custom-scripts-types/types/modules/event-manager";
 import {
     TILTIFY_EVENT_SOURCE_ID,
-    TILTIFY_DONATION_EVENT_ID
+    TILTIFY_DONATION_EVENT_ID,
+    TILTIFY_MILESTONE_EVENT_ID
 } from "../constants";
+import { TiltifyDonationEventData } from "./donation-event-data";
+import { TiltifyMilestoneReachedEventData } from "./milestone-reached-event-data";
 
 export const TiltifyEventSource: EventSource = {
     id: TILTIFY_EVENT_SOURCE_ID,
@@ -37,8 +40,39 @@ export const TiltifyEventSource: EventSource = {
             queued: true,
             activityFeed: {
                 icon: "fad fa-heart",
-                getMessage: (eventData: any) => {
-                    return `**${eventData.from}** donated **$${eventData.donationAmount}** to Tiltify${eventData.rewardName ? ` with reward *${eventData.rewardName}*` : eventData.rewardId && eventData.rewardId !== -1 ? ` with reward *${eventData.rewardId}*` : ""}`;
+                getMessage: (eventData: TiltifyDonationEventData) => {
+                    return `**${eventData.from}** donated **$${eventData.donationAmount}** to Tiltify${eventData.rewardName ? ` with reward *${eventData.rewardName}*` : eventData.rewardId ? ` with reward *${eventData.rewardId}*` : ""}`;
+                }
+            }
+        }, 
+        {
+            id: TILTIFY_MILESTONE_EVENT_ID,
+            name: "Milestone Reached",
+            description: "When a Milestone of your Tiltify campaign has been reached.",
+            cached: false,
+            manualMetadata: {
+                id: "",
+                name: "Awesome Milestone",
+                amount: 1000,
+                campaignInfo: {
+                    name: "GOTEL",
+                    cause: "Lupus Foundation of America",
+                    causeLegalName: "Lupus Foundation of America, Inc.",
+                    fundraisingGoal: 1000,
+                    originalGoal: 500,
+                    supportingRaised: 500,
+                    amountRaised: 1000,
+                    totalRaised: 1500
+                }
+            },
+            //@ts-ignore
+            isIntegration: true,
+            queued: true,
+            activityFeed: {
+                icon: "fad fa-heartbeat",
+                getMessage: (eventData: TiltifyMilestoneReachedEventData) => {
+                    return `Milestone **${eventData.name}** reached in campaign ${eventData.campaignInfo.name}. 
+Threshold: $${eventData.amount}`;
                 }
             }
         }
